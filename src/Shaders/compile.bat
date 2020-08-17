5db88083af834e00b65732ceaa16b144a5e23cea
Filip Husnjak <filip.husnjak@fer.hr>
2020-08-17 18:34:51 +0200

    Create shader for skeletal animation

--- src/Shaders/compile.bat
+++ src/Shaders/compile.bat
@@ -1,4 +1,4 @@
 "C:/VulkanSDK/1.2.131.2/Bin32/glslc.exe" shader_vert.vert -o vert.spv
+"C:/VulkanSDK/1.2.131.2/Bin32/glslc.exe" shader_vert_animation.vert -o animation_vert.spv
 "C:/VulkanSDK/1.2.131.2/Bin32/glslc.exe" shader_frag.frag -o frag.spv
-"C:/VulkanSDK/1.2.131.2/Bin32/glslc.exe" shader_post_vert.vert -o vert_post.spv
-"C:/VulkanSDK/1.2.131.2/Bin32/glslc.exe" shader_post_frag.frag -o frag_post.spv
+
